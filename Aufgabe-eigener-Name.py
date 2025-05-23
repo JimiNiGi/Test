0ba44c60513e8df5580ed001a891ff22493ff515
Nico <Warmohawk42@gmail.com>
2025-05-23 11:59:32 +0200

    Aufgabe-Nico

---
+++ Aufgabe-eigener-Name.py
@@ -0,0 +1,16 @@
+daten = {
+    "Name": "Nico Gärtner",
+    "Alter": 33,
+    "Geschlecht": "männlich",
+    "Größe": "1,83m",
+    "Gewicht": "72kg",
+    "Augenfarbe": "blau",
+    "Haarfarbe": "blond",
+    "Beruf": "System Administrator",
+    "Hobby": "zocken",
+    "Charaktereigenschaften": "humorvoll"
+}
+
+satz = f"{daten['Name']} ist {daten['Alter']} Jahre alt, {daten['Geschlecht']} und {daten['Größe']} groß. Er wiegt {daten['Gewicht']}kg, hat {daten['Augenfarbe']}e Augen und {daten['Haarfarbe']}e Haare. Von Beruf ist er {daten['Beruf']}, sein Hobby ist {daten['Hobby']} und er wird als {daten['Charaktereigenschaften']} beschrieben."
+
+print(satz)
