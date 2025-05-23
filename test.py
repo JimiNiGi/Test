9bacaeddcc8a991555d738d37077523becef1c8c
Nico <Warmohawk42@gmail.com>
2025-05-23 11:05:45 +0200

    Aufgabe

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print(4+8)
+print((5*20)-(40/5))
 print("Hello World")
+print('Die Programmiersprache "Python" ist cool')
+print('''Die Programmiersprache
+       "Python" 
+      ist cool''')
+age = 33
+print(age)
+age = 34
+name = "Nico"
+print(age)
+print(name)
+print("String"+"verkettung")
