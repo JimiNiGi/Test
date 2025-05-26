00e97abfc3f87542f7245422fa5878f2314886b7
Nico <Warmohawk42@gmail.com>
2025-05-26 15:42:30 +0200

    positiv-0-negativ

---
+++ Aufgabe-positiv-0-negativ.py
@@ -0,0 +1,9 @@
+
+zahl = int(input("Nenne mir deine Zahl: "))
+
+if zahl > 0:
+    print("Die Zahl ist positiv.")
+elif zahl < 0:
+    print("Die Zahl ist negativ.")
+else:
+    print("Die Zahl ist 0.")
