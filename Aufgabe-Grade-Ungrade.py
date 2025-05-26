4f8b00c1026c9a08f1710bbd9546c5689fbbee4f
Nico <Warmohawk42@gmail.com>
2025-05-26 15:35:01 +0200

    grade-ungrade

---
+++ Aufgabe-Grade-Ungrade.py
@@ -0,0 +1,6 @@
+zahl = int(input("Schreibe eine Zahl zwischen 1 und 100, wir testen ob sie grade oder ungrade ist!Zahl:"))
+
+if zahl % 2 == 0:
+    print("Zahl ist gerade")
+else:
+    print("Zahl ist ungerade")
