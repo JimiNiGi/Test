801100c5ad8e36150b9c16385edbc2aada52baca
Nico <Warmohawk42@gmail.com>
2025-05-26 11:18:20 +0200

    Video-7-Aufgabe

---
+++ Video-7.py
@@ -0,0 +1,19 @@
+value1 = input("Erste Zahl eingeben: ")
+value2 = input("Zweite Zahl eingeben: ")
+
+print(value1 + value2)
+
+value3 = input("Erste Zahl eingeben: ")
+value4 = input("Zweite Zahl eingeben: ")
+
+print(int(value3) + int(value4))
+
+value5 = int(input("Erste Zahl eingeben: "))
+value6 = int(input("Zweite Zahl eingeben: "))
+
+print(value5 + value6)
+
+value7 = float(input("Erste Zahl eingeben: "))
+value8 = float(input("Zweite Zahl eineben: "))
+
+print(value7 + value8)
