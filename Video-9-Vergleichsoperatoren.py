179140a3964be8825122744c66a5f87ffb7e5ad1
Nico <Warmohawk42@gmail.com>
2025-05-26 11:42:02 +0200

    Vergleichsoperatoren

---
+++ Video-9-Vergleichsoperatoren.py
@@ -0,0 +1,14 @@
+print(1 < 5) # kleiner als
+print(5 < 1)
+print(type(5 < 1))
+print(5 > 1) # größer als
+print(1 > 5)
+print(1 <= 5) # kleiner gleich als
+print(5 <= 5)
+print(6 <= 5)
+print(1 >= 5) # größer gleich als
+print(5 >= 5)
+print(6 >= 5)
+print( 5 == 5) # gleich
+print((4 + 2) * 3 == 18)
+print(5 != 5) # ungleich
