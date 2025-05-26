8df8bcfbaf5a36acd83b27aa4e3c0473efd1dac2
Nico <Warmohawk42@gmail.com>
2025-05-26 11:30:23 +0200

    Zuweisungs Operator

---
+++ Video-8-Zuweisungsoperator.py
@@ -0,0 +1,17 @@
+example1 = 22
+
+example2 = 22 + 3
+
+example3 = (22 - 2) * 3
+
+value1 = 5
+value2 = 6
+
+value1 = value1 + 10
+print(value1)
+
+value2 += 10
+print(value2)
+
+value2 /= 2     # value2 = value2 / 2
+print(value2)
