2258a334b6b37a8678af2aac8f20aa941ba132a8
Nico <Warmohawk42@gmail.com>
2025-05-26 13:28:25 +0200

    If-Anweisung

---
+++ Video-10-If-Anweisung.py
@@ -0,0 +1,14 @@
+
+age = int(input("Bitte gebe dein Alter ein: "))
+
+if age < 18:
+    print("Achtung, der Nutzer ist jünger als 18")
+    print("Das Programm zerstört sich in 3 Sekunden")
+    print("3")
+    print("2")
+    print("1")
+    print("Boom")
+
+if age >= 18:
+    print("Willkommen")
+    print("Wie kann ich Ihnen helfen?")
