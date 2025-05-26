cd188cf019002b11b358da3edb3294c82fa23bd8
Nico <Warmohawk42@gmail.com>
2025-05-26 13:13:53 +0200

    Type Casting

--- Video-7-TypeCasting.py
+++ Video-7-TypeCasting.py
@@ -1,19 +1,19 @@
-value1 = input("Erste Zahl eingeben: ")
+value1 = input("Erste Zahl eingeben: ") # String = Aneinanderreihung
 value2 = input("Zweite Zahl eingeben: ")
 
 print(value1 + value2)
 
-value3 = input("Erste Zahl eingeben: ")
+value3 = input("Erste Zahl eingeben: ") #ganze Zahlen
 value4 = input("Zweite Zahl eingeben: ")
 
 print(int(value3) + int(value4))
 
-value5 = int(input("Erste Zahl eingeben: "))
+value5 = int(input("Erste Zahl eingeben: "))  # ganze Zahlen
 value6 = int(input("Zweite Zahl eingeben: "))
 
 print(value5 + value6)
 
-value7 = float(input("Erste Zahl eingeben: "))
+value7 = float(input("Erste Zahl eingeben: ")) # float = fließkomma Zahlen
 value8 = float(input("Zweite Zahl eineben: "))
 
 print(value7 + value8)
