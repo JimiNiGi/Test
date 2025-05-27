6806908e3a239e6562a96ed6f0eef1d36eb0ae00
Nico <Warmohawk42@gmail.com>
2025-05-27 13:25:08 +0200

    Listen

---
+++ Videos Youtube/Video-14-Einführung-in-Listen.py
@@ -0,0 +1,9 @@
+
+test_variable = 10
+
+first_list = [3, 5, 15, 17, 20]     #Eckigeklammern ganz WICHTIG!
+
+print(type(test_variable))
+print(type(first_list))
+print(test_variable)
+print(first_list)
---
+++ Videos Youtube/Video-15-Zugriff-auf-Listen.py
@@ -0,0 +1,10 @@
+
+names = ["Nico", "Fritz", 5, 2.5]
+print(names)
+
+print(names[1])     #Aufzählungen starten immer bei 0
+print(names[2])     #die Zahl muss in eckigen Klammern sein
+print(names[-1])    #-1 zeigt das letzte Listenelement
+
+names[0] = "Peter"
+print(names)
